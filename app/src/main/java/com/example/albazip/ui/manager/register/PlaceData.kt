f81e719bea23aa10e9dfbdc7dd24ec5424830817
stopkite <62979643+stopkite@users.noreply.github.com>
2021-10-19 17:37:41 +0900

    [FEAT] 매장등록(지도) 레이아웃 배치 완료

---
+++ app/src/main/java/com/example/albazip/ui/manager/register/PlaceData.kt
@@ -0,0 +1,7 @@
+package com.example.albazip.ui.manager.register
+
+import android.graphics.drawable.Drawable
+import android.view.View
+import android.widget.RelativeLayout
+
+data class PlaceData(var name:String, var address:String, var flags:Boolean)
--- app/src/main/java/com/example/albazip/ui/manager/register/SearchPlaceActivity.kt
+++ app/src/main/java/com/example/albazip/ui/manager/register/SearchPlaceActivity.kt
@@ -1,13 +1,64 @@
 package com.example.albazip.ui.manager.register
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
+import androidx.core.view.allViews
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.albazip.R
 import com.example.albazip.config.BaseActivity
 import com.example.albazip.databinding.ActivitySearchPlaceBinding
+import com.example.albazip.ui.manager.register.adapter.PlaceListAdapter
 
-class SearchPlaceActivity : BaseActivity<ActivitySearchPlaceBinding>(ActivitySearchPlaceBinding::inflate) {
+class SearchPlaceActivity :
+    BaseActivity<ActivitySearchPlaceBinding>(ActivitySearchPlaceBinding::inflate) {
+
+    private lateinit var pAdapter: PlaceListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // 매장 리스트를 담기 위한 배열 생성
+        val itemList = ArrayList<PlaceData>()
+
+        itemList.add(PlaceData("서플라이 가", "광주 광산구 임방울대로826번길 47",false))
+        itemList.add(PlaceData("서플라이 나", "광주 서구 상무자유로 32",false))
+        itemList.add(PlaceData("서플라이 다", "광주 광산구 임방울대로826번길 47",false))
+        itemList.add(PlaceData("서플라이 라", "광주 서구 상무자유로 32",false))
+        itemList.add(PlaceData("서플라이 마", "광주 광산구 임방울대로826번길 47",false))
+        itemList.add(PlaceData("서플라이 바", "광주 서구 상무자유로 32",false))
+
+        pAdapter = PlaceListAdapter(this, itemList)
+
+        // 리사이클러 뷰 타입 설정
+        val linearLayoutManager = LinearLayoutManager(this)
+        linearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+
+        binding.rvPlace.layoutManager = linearLayoutManager
+
+        pAdapter.itemClick = object : PlaceListAdapter.ItemClick {
+
+            override fun onClick(view: View, position: Int) {
+
+
+                // 클릭 시 스크롤 이동
+                linearLayoutManager.scrollToPositionWithOffset(position, 0)
+
+                for(i in 0 until itemList.size){
+                    itemList[i].flags = i == position
+                }
+
+                pAdapter.notifyDataSetChanged()
+
+            }
+
+        }
+
+        // 만든 어댑터 recyclerview 에 연결
+        binding.rvPlace.adapter = pAdapter
+
     }
+
 }
