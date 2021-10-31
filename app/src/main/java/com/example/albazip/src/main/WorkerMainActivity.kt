24dec70c458af003e161593436d6d19299b513e8
stopkite <62979643+stopkite@users.noreply.github.com>
2021-10-31 14:21:42 +0900

    [FEAT] WEEK1 UI End

---
+++ app/src/main/java/com/example/albazip/src/main/WorkerMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.albazip.src.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.albazip.R
+
+class WorkerMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_worker_main)
+    }
+}
