d0e824c693d73e0277b90dc42846636fbc64df12
stopkite <62979643+stopkite@users.noreply.github.com>
2021-11-01 20:15:08 +0900

    [STYLE] 마이페이지 UI 동작 구현 완료

---
+++ app/src/main/java/com/example/albazip/src/home/manager/WorkerListChildFragment.kt
@@ -0,0 +1,24 @@
+package com.example.albazip.src.home.manager
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.albazip.R
+import com.example.albazip.config.BaseFragment
+import com.example.albazip.databinding.ChildFragmentWorkerListBinding
+
+class WorkerListChildFragment : BaseFragment<ChildFragmentWorkerListBinding>(
+    ChildFragmentWorkerListBinding::bind,
+    R.layout.child_fragment_worker_list
+) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.swipelayout.setOnRefreshListener(SwipeRefreshLayout.OnRefreshListener {
+            binding.swipelayout.isRefreshing = false
+            showCustomToast("새로고침 완료")
+        })
+    }
+}
