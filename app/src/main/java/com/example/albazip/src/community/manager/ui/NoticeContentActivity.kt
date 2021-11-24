fe31c9cb0d19bf993cc27f7437916372ee6054e5
stopkite <62979643+stopkite@users.noreply.github.com>
2021-11-24 20:46:35 +0900

    [STYLE] 커뮤니티 UI 완성

---
+++ app/src/main/java/com/example/albazip/src/community/manager/ui/NoticeContentActivity.kt
@@ -0,0 +1,13 @@
+package com.example.albazip.src.community.manager.ui
+
+import android.os.Bundle
+import com.example.albazip.config.BaseActivity
+import com.example.albazip.databinding.ActivityNoticeContentBinding
+
+// 공지글
+class NoticeContentActivity:BaseActivity<ActivityNoticeContentBinding>(ActivityNoticeContentBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
