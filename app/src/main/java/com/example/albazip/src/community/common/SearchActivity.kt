687319f3f3bdf691b61c06aec4b007fdd0a76096
stopkite <62979643+stopkite@users.noreply.github.com>
2021-11-24 21:21:12 +0900

    [STYLE] 커뮤니티 UI 완성(검색 포함)

---
+++ app/src/main/java/com/example/albazip/src/community/common/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.example.albazip.src.community.common
+
+import android.os.Bundle
+import com.example.albazip.config.BaseActivity
+import com.example.albazip.databinding.ActivitySearchBinding
+
+class SearchActivity:BaseActivity<ActivitySearchBinding>(ActivitySearchBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
