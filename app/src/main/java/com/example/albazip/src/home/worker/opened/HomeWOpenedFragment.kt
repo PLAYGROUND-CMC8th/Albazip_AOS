b5bd781a534423f5ea66d2ee39a8a21d7ee1c2be
stopkite <62979643+stopkite@users.noreply.github.com>
2021-11-29 12:02:34 +0900

    [FIX] QR 찍고 홈화면 갱신시키기

--- app/src/main/java/com/example/albazip/src/home/worker/opened/HomeWOpenedFragment.kt
+++ app/src/main/java/com/example/albazip/src/home/worker/opened/HomeWOpenedFragment.kt
@@ -18,6 +18,7 @@ import com.example.albazip.src.home.worker.data.AllHomeWResult
 import com.example.albazip.src.home.worker.network.PutQRScanFragmentView
 import com.example.albazip.src.home.worker.network.PutQRScanService
 import com.example.albazip.src.home.worker.opened.worklist.ui.HomeWTodayToDoListActivity
+import com.example.albazip.src.main.WorkerMainActivity
 import com.example.albazip.util.GetCurrentTime
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanIntentResult
@@ -202,6 +203,11 @@ class HomeWOpenedFragment(data: AllHomeWResult) : BaseFragment<ChildFragmentHome
         }else{
             showCustomToast(GetCurrentTime().getTime+"에 퇴근이 기록되었습니다.")
         }
+
+        // 다시 홈화면으로 이동
+        val nextIntent = Intent(requireContext(),WorkerMainActivity::class.java)
+        startActivity(nextIntent)
+        activity?.finishAffinity()
     }
     // QR 스캔 실패
     override fun onPutQRFailure(message: String) {
