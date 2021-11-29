c27b179cf76ff31214a7bd0c38890e74743654d6
stopkite <62979643+stopkite@users.noreply.github.com>
2021-11-29 17:35:48 +0900

    [FIX] 근무자 설정창 버튼 테스트 완료

--- app/src/main/java/com/example/albazip/src/mypage/common/setting/WMyPageSettingActivity.kt
+++ app/src/main/java/com/example/albazip/src/mypage/common/setting/WMyPageSettingActivity.kt
@@ -6,15 +6,33 @@ import com.example.albazip.config.BaseActivity
 import com.example.albazip.databinding.ActivitySettingWMyPageBinding
 import com.example.albazip.src.mypage.common.setting.editinfo.ManageMyInfoActivity
 import com.example.albazip.src.mypage.custom.LogoutBottomSheetDialog
+import com.example.albazip.src.withdraw.ui.DefaultWithDrawActivity
 
 class WMyPageSettingActivity:BaseActivity<ActivitySettingWMyPageBinding>(ActivitySettingWMyPageBinding::inflate){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // 뒤로가기 화면
+        binding.ibtnClose.setOnClickListener {
+            finish()
+        }
+
         // 알림 설정 화면
         binding.rlAlarmSettingBtn.setOnClickListener {
             val nextIntent = Intent(this, AlarmSettingActivity::class.java)
+            startActivity(nextIntent)
+        }
+
+        // 공지사항 화면
+        binding.rlNotice.setOnClickListener {
+            val nextIntent = Intent(this, AppNoticeActivity::class.java)
+            startActivity(nextIntent)
+        }
+
+        // 서비스 이용약관
+        binding.rlService.setOnClickListener {
+            val nextIntent = Intent(this,ServiceLawActivity::class.java)
             startActivity(nextIntent)
         }
 
@@ -26,6 +44,12 @@ class WMyPageSettingActivity:BaseActivity<ActivitySettingWMyPageBinding>(Activit
         // 내정보관리 화면
         binding.rlManageMyinfo.setOnClickListener {
             val nextIntent = Intent(this, ManageMyInfoActivity::class.java)
+            startActivity(nextIntent)
+        }
+
+        // 회원탈퇴
+        binding.rlWithdraw.setOnClickListener {
+            val nextIntent = Intent(this, DefaultWithDrawActivity::class.java)
             startActivity(nextIntent)
         }
     }
