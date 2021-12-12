e0c0bb484a1add051832cd585effa2beb7dfbe63
stopkite <62979643+stopkite@users.noreply.github.com>
2021-12-12 17:39:42 +0900

    [FEAT] 공지사항 검색기능 추가(관리자 버전)

--- app/src/main/java/com/playground/albazip/src/community/worker/WCommunityFragment.kt
+++ app/src/main/java/com/playground/albazip/src/community/worker/WCommunityFragment.kt
@@ -1,5 +1,6 @@
 package com.playground.albazip.src.community.worker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -11,6 +12,8 @@ import com.playground.albazip.databinding.FragmentWCommunityBinding
 import com.playground.albazip.src.community.manager.ui.NoticeMChildFragment
 import com.playground.albazip.src.community.worker.ui.NoticeWChildFragment
 import com.google.android.material.tabs.TabLayoutMediator
+import com.playground.albazip.src.community.manager.ui.SearchCoMActivity
+import com.playground.albazip.src.home.common.ui.HomeAlarmActivity
 
 class WCommunityFragment :
     BaseFragment<FragmentWCommunityBinding>(FragmentWCommunityBinding::bind, R.layout.fragment_w_community) {
@@ -22,6 +25,18 @@ class WCommunityFragment :
 
         // 탭 레이아웃 초기화
         init()
+
+        // 검색화면으로 이동
+        binding.tbrIbtnSearch.setOnClickListener {
+            val nextIntent = Intent(requireContext(), SearchCoMActivity::class.java)
+            startActivity(nextIntent)
+        }
+
+        // 알림 화면으로 이동(준비중)
+        binding.tbrIbtnNotice.setOnClickListener {
+            val nextIntent = Intent(requireContext(), HomeAlarmActivity::class.java)
+            startActivity(nextIntent)
+        }
     }
 
 
