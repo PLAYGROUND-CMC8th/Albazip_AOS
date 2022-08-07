693e7d279d177cc51949b3bc962dd2fcfad691db
kite <stopkite98@naver.com>
2022-08-07 13:35:27 +0900

    [feat] 매장 추가 정보 UI 변경

---
+++ app/src/main/java/com/playground/albazip/src/register/manager/place/InputPlaceMoreBetaActivity.kt
@@ -0,0 +1,11 @@
+package com.playground.albazip.src.register.manager.place
+
+import android.os.Bundle
+import com.playground.albazip.config.BaseActivity
+import com.playground.albazip.databinding.ActivityInputPlaceMoreBetaBinding
+
+class InputPlaceMoreBetaActivity: BaseActivity<ActivityInputPlaceMoreBetaBinding>(ActivityInputPlaceMoreBetaBinding::inflate){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
