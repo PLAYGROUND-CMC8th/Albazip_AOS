b850ab8298a71a4b6ca426748c5c6a621f330b2f
kite <stopkite98@naver.com>
2022-09-11 15:31:39 +0900

    #17 [fix] 근무자 관리 이후 추가 수정 예정

--- app/src/main/java/com/playground/albazip/src/register/manager/moreinfo/custom/RunningTimePickerBottomSheetDialog.kt
+++ app/src/main/java/com/playground/albazip/src/register/manager/moreinfo/custom/RunningTimePickerBottomSheetDialog.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.TimePicker
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.playground.albazip.databinding.DialogFragmentTimeBinding
